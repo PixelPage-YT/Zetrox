266de5912d85bbe09265b82b0b33f62ccdb63671
Kloetzchengrafikk <pascal.adk@icloud.com>
2022-04-01 19:43:11 +0200

    fix(noPerms): Keine Message (Notlösung)

--- util/noPerms.ts
+++ util/noPerms.ts
@@ -8,29 +8,29 @@ export async function noPerms(i:harmony.Interaction,error:string){
             if(logChannel && logChannel.isText()){
                 await logChannel.send(`ErrorCode: ${errorCode} | Jemand hat versucht, den Befehl ${i.isApplicationCommand() ? i.name : i.isMessageComponent() ? i.customID : "[unbekannt]"} zu nutzen, hat aber den folgenden Fehler erhalten: ${error}`)
             }
-            await i.channel?.send({
-                embeds: [
-                    {
-                        "title": `:warning: Da ist etwas schiefgelaufen! (${errorCode}) :warning:`,
-                        "description": "*Dies kann an mehreren Sachen liegen.*",
-                        "color": 13750554,
-                        "fields": [
-                            {
-                                "name": "Berechtigungen",
-                                "value": "Achte darauf, dass der Bot die richtigen Berechtigungen hat.\nFalls das nicht der Fall ist, kannst du dem Bot [hier](https://dsc.gg/zetroxbot) die Berechtigungen geben."
-                            },
-                            {
-                                "name": "Anderes",
-                                "value": "Falls es nicht daran liegt, melde dich bitte auf [unserem Server](https://discord.gg/mAFaUnT)."
-                            }
-                        ],
-                        "footer": {
-                            "text": "⇢ Zetrox von Folizza Studios",
-                            "icon_url": "https://sph-download.neocities.org/share/GoDaddyStudioPage-0%202.png"
-                        }
-                    }
-                ]
-            })
+            // await i.channel?.send({
+            //     embeds: [
+            //         {
+            //             "title": `:warning: Da ist etwas schiefgelaufen! (${errorCode}) :warning:`,
+            //             "description": "*Dies kann an mehreren Sachen liegen.*",
+            //             "color": 13750554,
+            //             "fields": [
+            //                 {
+            //                     "name": "Berechtigungen",
+            //                     "value": "Achte darauf, dass der Bot die richtigen Berechtigungen hat.\nFalls das nicht der Fall ist, kannst du dem Bot [hier](https://dsc.gg/zetroxbot) die Berechtigungen geben."
+            //                 },
+            //                 {
+            //                     "name": "Anderes",
+            //                     "value": "Falls es nicht daran liegt, melde dich bitte auf [unserem Server](https://discord.gg/mAFaUnT)."
+            //                 }
+            //             ],
+            //             "footer": {
+            //                 "text": "⇢ Zetrox von Folizza Studios",
+            //                 "icon_url": "https://sph-download.neocities.org/share/GoDaddyStudioPage-0%202.png"
+            //             }
+            //         }
+            //     ]
+            // })
         }
     }catch(err){
 
